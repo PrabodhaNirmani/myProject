0dbfa1e62e553cfe8229e8fa26d42148d78a9b40
saarrahisthikar <saarrahisthikar@gmail.com>
2016-12-18 15:52:48 +0530

    getApplicationList route

--- app/Http/Controllers/SchoolController.php
+++ app/Http/Controllers/SchoolController.php
@@ -25,10 +25,22 @@ class SchoolController extends Controller
     }
 
     public function getApplicantList(){
-        $applicants=null;
+       $connection =DatabaseController::db_connect();
+        $user =Auth::user()->user_id;
+        $query ="SELECT applicant.applicant_id,applicant.first_name,applicant.last_name from applicant, applicant_priority where (applicant.applicant_id, applicant.school_id)=(applicant_priority,$user)";
+            $result = mysqli_query($connection,$query);
+        $applicants = array();
+        if(mysqli_num_rows($result)>0){
+            while (($row=mysqli_fetch_row($result))){
+                array_push($applicants,$row);
+            }}
+            else{
+                $error = "No Applicants found";
+                $applicants=null;
+            }
+            return view('searchApplicant', compact('applicants', 'error'));
+        }
 
-        return view('applicantList',compact('applicants'));
-    }
 
     public function postGetApplication(Request $request){
 //        $district_row=District::getDistrict();
