b3637216a7fedbaae995f2f845cf34411ea01ebf
ThirasaraDevanmini <thirasaradevanmini@gmail.com>
2016-12-18 17:38:46 +0530

    error class

---
+++ app/Models/Error.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ThirasaraDevanmini
+ * Date: 12/18/2016
+ * Time: 5:15 PM
+ */
+
+namespace App\Models;
+
+
+class Error
+{
+    public $error_description;
+    public $error_no;
+    public function __construct($error,$error_no)
+    {
+        $this->error_description = $error;
+        $this->error_no = $error_no;
+    }
+
+    public function getErrorDescription()
+    {
+        return $this->error_description;
+    }
+
+    public function getErrorNo()
+    {
+        return $this->error_no;
+    }
+    
+}
