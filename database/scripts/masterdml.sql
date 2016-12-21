9350e0ebbb4957c3b3ba60acffbaac7d65f71512
saarrahisthikar <saarrahisthikar@gmail.com>
2016-12-21 10:19:14 +0530

    Creating function to calculate marks of a student

---
+++ database/scripts/masterdml.sql
@@ -0,0 +1,3 @@
+----------Adding Districts to District Table-------------------------------------------------------------------------
+
+insert into district (city) values('Matara'),('Galle'),('Hambantota'),('Colombo'),('Gampaha'),('Kaluthara'),('Monaragala'),('Badulla'),('Kandy'),('Matale'),('Nuwara Eliya'),('Ampara'),('Anuradhapura'),('Batticaloa'),('Jaffna'),('Kegalle'),('Kilinochchi'),('Kurunegala'),('Mannar'),('Mullaitivu'),('Polonnaruwa'),('Puttalam'),('Rathnapura'),('Trincomalee'),('Vavniya');
